a4ab30a6652b64431ab33af9a63e46f3b8aa3395
TheGreekCuban <theodorealexander92@gmaill.com>
2019-10-16 15:54:40 -0400

    set up folder structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Alexander-Vellios-Portfolio
-My professional portfolio containing an about me section and a compilation of all my latest projects and mini-projects.
+# Alexander Vellios Portfolio
+## My professional portfolio containing an about me section and a compilation of all my latest projects and mini-projects.
+## This application is powered by Materialize, CSS and HTML.
+
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+
+//Save the initialization of express to the app variable
+const app = express()
+
+//Create a variable that willl be the PORT
+const PORT = process.env.PORT || 3030
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Import the routes and give the server access to them
+const routes = require("./controllers")
+
+app.use(routes)
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+  console.log("App listening on http://localhost:" + PORT);
+});
