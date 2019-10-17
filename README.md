9b3aad38b13b0d4d1749d9a4cf7a9fb1c5bdb0cc
TheGreekCuban <theodorealexander92@gmaill.com>
2019-10-16 22:09:05 -0400

    all files now there

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ### Link: https://vellios-professional-portfolio.herokuapp.com/
 ### My professional portfolio containing an about me section and a compilation of all my latest projects and mini-projects.
 ### This application is powered by Node.js, Express.js, Materialize, CSS and HTML.
-#### Technologies Used In These Projects: 
+##### Technologies Used In These Projects: 
 ##### Front End: HTML5 | CSS3 | Javascript | JQuery | Bootstrap | Materialize | Handlebars.js | React.js
 ##### Back End: Node.js | MySQL | MongoDB | Express.js | Sequelize.js | Model View Controller Format | ORM
 
--- controllers/index.js
+++ controllers/index.js
@@ -6,4 +6,23 @@ router.get("/", (request, response) => {
     response.sendFile(path.join(__dirname, "../public/html/aboutme.html"))
 })
 
+router.get("/aboutme.html", (request, response) => {
+    response.sendFile(path.join(__dirname, "../public/html/aboutme.html"))
+})
+
+router.get("/projects.html", (request, response) => {
+    response.sendFile(path.join(__dirname, "../public/html/projects.html"))
+})
+
+router.get("/mini-projects.html", (request, response) => {
+    response.sendFile(path.join(__dirname, "../public/html/mini-projects.html"))
+})
+
+router.get("/contact.html", (request, response) => {
+    response.sendFile(path.join(__dirname, "../public/html/contact.html"))
+})
+
+router.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "../public/html/aboutme.html"))
+})
 module.exports = router
