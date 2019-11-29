1233ff94000ebb1cb950b8f3c92a5e09741bf00a
TheGreekCuban <theodorealexander92@gmaill.com>
2019-11-29 00:45:17 -0500

    fixed projects page

--- public/js/projects.js
+++ public/js/projects.js
@@ -2,11 +2,10 @@ $(document).ready(function(){
   
     $('.sidenav').sidenav();
     $('.carousel').carousel({
-      indicators: true,
       padding: 15,
       numVisible: 3,
       duration: 400,
-      pressed: true
+      pressed: true,
     });
 
   });
