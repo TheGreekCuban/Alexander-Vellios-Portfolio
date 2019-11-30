9bf4cc49cf5d392b2a30d824ea60764b78ed1667
TheGreekCuban <theodorealexander92@gmaill.com>
2019-11-30 15:39:12 -0500

    fully functioning, now just need to add descriptions and tech

--- public/js/projects.js
+++ public/js/projects.js
@@ -10,13 +10,32 @@ $(document).ready(function(){
 
    
 
+    let activeElement = null
 
-    $(".carousel").click(function(event) {
-      console.log("hello")
-      let log = $(this).find(".active").attr("itemid")
-      console.log(log)
-    })
+    function activeElementChanged(newElement) {
+        let oldElementId = $(activeElement).find("img.responsive-img").attr("itemid")
+        activeElement = newElement
+        let itemId = $(newElement).find("img.responsive-img").attr("itemid")
+        changeDescriptionText(itemId, oldElementId)
+    }
+    
+    function changeDescriptionText(itemId, oldElementId) {
+      console.log(itemId, oldElementId)
+      $("#testp" + oldElementId).css("display", "none")
+      $("#testp" + itemId).css("display", "inline")
+    }
 
-  
+    //This is a helper function that listens for the change in the current active element. First it saves the active element to currentActiveElement. If that variable is not = to the activeElement, the active element change function it called. Otherwise the requestAnimationTimeFrame function is continuously iterating, waiting for the change.
+    let activeElementListener = function () {
+        let currentActiveElement = $("a.carousel-item.active").get(0)
+        if(currentActiveElement !== activeElement) {
+            activeElementChanged(currentActiveElement)
+        }
+        requestAnimationFrame(activeElementListener)
+    }
 
+    //This method tells the browser that you wish to perform an animation and requests that the browser calls a specified function to update an animation before the next repaint. The method takes a callback as an argument to be invoked before the repaint.
+
+    //Note: Your callback routine must itself call requestAnimationFrame() if you want to animate another frame at the next repaint.
+    requestAnimationFrame(activeElementListener)
   });
