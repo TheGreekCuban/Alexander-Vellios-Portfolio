d8683ff9ab7e9140fe4573171854e18dc0efc132
TheGreekCuban <theodorealexander92@gmaill.com>
2020-01-18 13:14:03 -0500

    got frontend portion of form working, need to add post

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+//Submit form function  
+const submitForm = (e) => {
+  e.preventDefault();
+  
+  //Get All Form Values
+  const name = getInputVal("first_name")
+  const phone = getInputVal("icon_telephone")
+  const email = getInputVal("email")
+  const message = getInputVal("textarea")
+
+  console.log(name)
+}
+
+//Function to get form values
+const getInputVal = id => {
+  return document.getElementById(id).value;
+}
+
+//Listen for form Submit  
+document.getElementById("cForm").addEventListener("submit", submitForm);
+
